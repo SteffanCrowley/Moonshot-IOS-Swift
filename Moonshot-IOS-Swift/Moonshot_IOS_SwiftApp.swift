029747d1d8b4ea292bf59e5d762d5c622b00378a
Steffan Crowley <steffancrow@gmail.com>
2022-08-23 15:05:48 -0700

    Initial Commit

---
+++ Moonshot-IOS-Swift/Moonshot_IOS_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_IOS_SwiftApp.swift
+//  Moonshot-IOS-Swift
+//
+//  Created by steffan crowley on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_IOS_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
